9604654e331cbcb3dee92e02dfe39959b6e4e49c
cnyballk <584518260@qq.com>
2018-12-11 16:48:52 +0800

    fix: 修复page初始化show无调用问题

--- dist/mini.js
+++ dist/mini.js
@@ -6,12 +6,10 @@ export const orm = (mapState, mapMethods) => pageConfig => {
   const app = getApp();
   const store = app.store;
   let __isHide__ = false;
-  let __isLoad__ = false;
   let update;
   let oldState;
   const {
     onLoad: _onLoad = () => {},
-    onUnload: _onUnload = () => {},
     onShow: _onShow = () => {},
     onHide: _onHide = () => {},
   } = pageConfig;
@@ -20,24 +18,15 @@ export const orm = (mapState, mapMethods) => pageConfig => {
     update = function(options, cb) {
       const state = store.state;
       const newState = mapState(state, options);
-      if (!equal(oldState, newState) || (!__isLoad__ || !__isHide__)) {
-        this.setData(newState, () => {
-          if (cb) {
-            cb(options);
-            __isLoad__ = true;
-          }
-        });
+      if (!equal(oldState, newState) || !__isHide__) {
+        this.setData(newState, () => cb && cb(options));
         oldState = newState;
       }
     }.bind(this, options);
-
-    store.listen(update);
-    update.call(this, _onLoad.bind(this));
+    _onLoad.call(this, options);
   }
 
   function onShow() {
-    if (!__isHide__) return;
-
     __isHide__ = false;
     store.listen(update);
     update.call(this, _onShow.bind(this));
@@ -48,16 +37,8 @@ export const orm = (mapState, mapMethods) => pageConfig => {
     _onHide.call(this);
     store.unListen(update);
   }
-
-  function onUnload() {
-    _onUnload.call(this);
-    __isLoad__ = false;
-    store.unListen(update);
-  }
-
   return assign({}, pageConfig, mapMethods(app.store.methods), {
     onLoad,
-    onUnload,
     onShow,
     onHide,
   });
@@ -131,7 +112,6 @@ export const ormComp = (mapState, mapMethods) => compConfig => {
     }),
   });
 };
-
 ////////////////////////////////////////////////////////////////
 //////////////// Provider
 export const Provider = store => appConfig => assign({}, appConfig, { store });
